04ba03962e3e2d2f87fd52f81cdf1af924bb604f
yxc <1335909103.@qq.com>
2020-02-13 11:19:20 +0800

    updata

--- end/demo1/bookdemo/booktest/admin.py
+++ end/demo1/bookdemo/booktest/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
 # Register your models here.
 
 
 # 注册自己需要管理的模型
 from .models import Book,Hero
-admin.site.register(Book)
-admin.site.register(Hero)
+
+
+class HeroInline(admin.StackedInline):
+    model=Hero
+    extra=1
+
+class HeroAdmin(ModelAdmin):
+
+    list_display = ('name','gender','content')
+
+admin.site.register(Hero,HeroAdmin)
+
+
+class BookAdmin(ModelAdmin):
+
+    list_display = ('title','price','pub_date')
+
+    inlines = [HeroInline]
+
+admin.site.register(Book,BookAdmin)
