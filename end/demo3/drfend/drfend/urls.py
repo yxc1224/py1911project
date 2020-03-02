4f80eeff9ad3dc188dd978ee245d6988e32bfcdf
yxc <1335909103.@qq.com>
2020-03-02 17:42:01 +0800

    基于类的视图

--- end/demo3/drfend/drfend/urls.py
+++ end/demo3/drfend/drfend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls import url
 from .settings import MEDIA_ROOT
 from shop.views import *
@@ -23,24 +23,30 @@ from django.views.static import serve
 #  引入API文档路由
 from rest_framework.documentation import include_docs_urls
 
-
 # 引入DRF自带的路由类
 from rest_framework import routers
-router=routers.DefaultRouter()
 
-router.register('categorys',CategoryViewSets)
-router.register('goods',GoodViewSets)
-router.register('goodimgs',GoodImgsViewSets)
+router = routers.DefaultRouter()
 
+router.register('categorys', CategoryViewSets)
+router.register('goods', GoodViewSets)
+router.register('goodimgs', GoodImgsViewSets)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    url( 'media/(?P<path>.*)',serve,{'document_root':MEDIA_ROOT} ),
+    url('media/(?P<path>.*)', serve, {'document_root': MEDIA_ROOT}),
 
-    url(r'^categorylist/$',categoryList,name='categorylist'),
-    url(r'^categorydetail/$',categoryDetail,name='categorydetail'),
+    # 基于函数的路由
+    # url(r'^categorylist/$', categoryList, name='categorylist'),
+    # url(r'^categorydetail/$', categoryDetail, name='categorydetail'),
+
+    # 基于类的路由
+    url(r'^categorylist/$', CategoryListView.as_view(), name='categorylist'),
+    url(r'^categorydetail/$', CategoryDetailView.as_view(), name='categorydetail'),
+
+
     # path('api/v1/',include(router.urls)),
-    path('api/v1/docs/',include_docs_urls(title="RestFulAPI",description='RestFulAPIv1')),
-    path('',include('rest_framework.urls')),
+    path('api/v1/docs/', include_docs_urls(title="RestFulAPI", description='RestFulAPIv1')),
+    path('', include('rest_framework.urls')),
 ]
--- end/demo3/drfend/shop/views.py
+++ end/demo3/drfend/shop/views.py
@@ -8,28 +8,78 @@ from django.http import HttpResponse, JsonResponse
 # 通过api_view装饰器  可以将基于函数的视图转换成 APIView基于类视图
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from django.shortcuts import get_object_or_404
+
+from django.views import View
+
+
+class CategoryListView(View):
+    """
+    继承Django自带的View类需要重写对应的http方法
+    """
+
+    def get(self, request):
+        return HttpResponse("返回列表成功")
+
+    def post(self, request):
+        return HttpResponse("创建成功")
+
+
+class CategoryDetailView(View):
+    def get(self, request, cid):
+        return HttpResponse("返回单个对象")
+
+    def put(self, request, cid):
+        return HttpResponse("修改成功put")
+
+    def patch(self, request, cid):
+        return HttpResponse("修改成功patch")
+
+    def delete(self, request, cid):
+        return HttpResponse("删除成功")
 
 
 @api_view(['GET', 'POST'])
 def categoryList(request):
-    print(request, type(request))
     if request.method == "GET":
         print("获取到GET请求参数", request.query_params)
-        return HttpResponse("获取列表成功")
+
+        # instance 为需要序列化的对象，来源于数据库
+        queryset = Category.objects.all()
+        seria = CategorySerializer(instance=queryset, many=True)
+        print(seria.data)
+        return Response(seria.data, status=status.HTTP_200_OK)
     elif request.method == "POST":
-        print("获取到POST请求参数", request.data)
-        return HttpResponse("创建成功")
+        # data 为序列化对象  来源于从请求中提取的数据
+        queryset = request.data
+        seria = CategorySerializer(data=queryset)
+        # 从请求中提取的数据 序列化之前需要进行校验
+        if seria.is_valid():
+            seria.save()
+            return Response(seria.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(seria.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def categoryDetail(request, cid):
+    model = get_object_or_404(Category, pk=cid)
     if request.method == "GET":
-        print("获取到GET请求参数", request.query_params)
-        return HttpResponse("获取单个成功")
+        seria = CategorySerializer(model)
+        return Response(seria.data, status=status.HTTP_200_OK)
     elif request.method == "PUT" or request.method == "PATCH":
-        print("获取到PUT/PATCH请求参数", request.data)
-        return HttpResponse("修改成功")
+        #  从请求中提取参数  替换掉从数据库中取出的数据
+        seria = CategorySerializer(instance=model, data=request.data)
+        if seria.is_valid():
+            seria.save()
+            return Response(seria.data, status.HTTP_200_OK)
+        else:
+            return Response(seria.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
-        return HttpResponse("删除成功")
+        model.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
     else:
         return HttpResponse('当前路由不允许' + request.method + '操作')
 
